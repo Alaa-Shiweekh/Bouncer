f2f7ada82060ea59548520fd61a075262b006c06
Alaa Mahmoud <alaashiweekh2811@gmail.com>
2026-09-04 09:46:27 +0300

    Delete blog_list.js

--- blog_list.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.page').forEach(item => {
-    item.addEventListener('click', function () {
-        document.querySelector('.active')?.classList.remove('active');
-        this.classList.add('active');
-    });
-});
